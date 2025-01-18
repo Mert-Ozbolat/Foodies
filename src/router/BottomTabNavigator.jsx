b25a50a0a2ab40ab97f0dfb5cdbfe99497b195be
Mert-Ozbolat <mertozbolat008@gmail.com>
2025-01-18 22:26:01 +0300

    update

--- src/router/BottomTabNavigator.jsx
+++ src/router/BottomTabNavigator.jsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BASKET, HOME } from '../utils/routes';
 import HomePage from '../screens/HomePage';
 import BasketPage from '../screens/BasketPage';
+import { Image, View } from 'react-native';
+import { colors } from '../theme/colors';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,14 +15,43 @@ const BottomTabs = () => {
             screenOptions={{
                 headerShown: false,
                 tabBarStyle: {
-                    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+                    backgroundColor: '#000', // Arka plan siyah yapıld
+                    height: 60, // Yükseklik artırıldı
                 },
-                tabBarActiveTintColor: '#007bff',
-                tabBarInactiveTintColor: '#6c757d',
             }}
         >
-            <Tab.Screen name={HOME} component={HomePage} />
-            <Tab.Screen name={BASKET} component={BasketPage} />
+            <Tab.Screen
+                name={HOME}
+                component={HomePage}
+                options={{
+                    tabBarLabel: '',
+                    tabBarIcon: () => (
+                        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                            <Image
+                                source={require('../assets/icons/navigation/home.png')}
+                                style={{ width: 25, height: 25 }}
+                                resizeMode="contain"
+                            />
+                        </View>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name={BASKET}
+                component={BasketPage}
+                options={{
+                    tabBarLabel: '',
+                    tabBarIcon: () => (
+                        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                            <Image
+                                source={require('../assets/icons/navigation/email.png')}
+                                style={{ width: 25, height: 25 }}
+                                resizeMode="contain"
+                            />
+                        </View>
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 };
